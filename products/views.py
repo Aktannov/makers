3ba6d5599c4793628999019fc12c76658042c0cc
chethahernya <povvezzlo@gmail.com>
2021-12-23 17:10:36 +0600

    accaunt + products

--- products/views.py
+++ products/views.py
@@ -2,9 +2,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 
-from .filters import ProductFilter
-from .models import Product, Category
-from .serializers import ProductSerializer, CategorySerializer
+from products.filters import ProductFilter
+from products.models import Product, Category
+from products.serializer import ProductSerializer, CategorySerializer
 
 
 class ProductViewSet(ModelViewSet):

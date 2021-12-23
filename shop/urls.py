dce08ba44699341001c5445bb60872fbaa00ca5a
Tali10 <smilestalgatbabaev@gmail.com>
2021-12-23 19:30:57 +0600

    add_swagger

--- shop/urls.py
+++ shop/urls.py
@@ -16,11 +16,23 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from shop import settings
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='makersshop',
+        default_version='v1',
+        description='Интернет магазин'
+    ),
+    public=True
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/docs/', schema_view.with_ui('swagger')),
     path('api/v1/', include('account.urls')),
     path('api/v1/', include('products.urls')),
 ]
